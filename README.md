f006ae42e52b595a2c175ae9b4baba44ea0c7663
agonyz <agonyz@outlook.de>
2022-12-17 17:41:35 +0100

    Initial commit for extension

---
+++ README.md
@@ -0,0 +1,11 @@
+## Amazon Price Game
+This is a tool to hide amazon prices. You can play a guessing game with this extension.
+
+### Install
+In Chrome navigate to:
+chrome://extensions/
+
+Drag and drop the extension folder into your browser.
+You can disable the extension if you want to show the prices again.
+
+Thanks to https://github.com/rufreakde/hide-amazon-prices for the idea!
---
+++ content.js
@@ -0,0 +1,119 @@
+const myStyleElement = document.createElement("style");
+const cssRuleElement = `span[class*="price"]{
+color: transparent!important;
+text-shadow: rgba(0, 0, 0, 0.8) 10px 0px 10px !important;
+border-style: dotted;
+border-color: #f23838;
+}
+`;
+const cssRuleElementExtended = `span[class*="Price"]{
+color: transparent!important;
+text-shadow: rgba(0, 0, 0, 0.8) 10px 0px 10px !important;
+border-style: dotted;
+border-color: #f23838;
+}
+`;
+const cssRulePricePerPiece = `span[class*="a-size-base a-color-secondary"]{
+color: transparent!important;
+text-shadow: rgba(0, 0, 0, 0.8) 10px 0px 10px !important;
+border-style: dotted;
+border-color: #f23838;
+}
+`;
+const cssRuleSubscribeAndSaveDiscount = `div[class*="a-row a-size-base a-color-secondary"]{
+color: transparent!important;
+text-shadow: rgba(0, 0, 0, 0.8) 10px 0px 10px !important;
+border-style: dotted;
+border-color: #f23838;
+}
+`;
+
+let extensionEnabled = true;
+
+const setEnabled = (boolean) => {
+    extensionEnabled = boolean;
+}
+
+const overlayOn = () => {
+    overlaySet('block');
+}
+
+const overlayOff = () => {
+    overlaySet('none');
+}
+
+const overlaySet = (displayStyle) => {
+    createOverlay();
+    if (document.getElementById("hide-overlay")) {
+        document.getElementById("hide-overlay").style.display = displayStyle;
+    }
+}
+
+const hidePrice = () => {
+    document.head.appendChild(myStyleElement);
+    myStyleElement.sheet.insertRule(cssRuleElement, 0);
+    myStyleElement.sheet.insertRule(cssRuleElementExtended, 1);
+    myStyleElement.sheet.insertRule(cssRulePricePerPiece, 2);
+    myStyleElement.sheet.insertRule(cssRuleSubscribeAndSaveDiscount, 3);
+}
+
+const showPrice = () => {
+    document.head.appendChild(myStyleElement);
+    myStyleElement.sheet.deleteRule(0)
+    myStyleElement.sheet.deleteRule(1)
+    myStyleElement.sheet.deleteRule(2)
+    myStyleElement.sheet.deleteRule(3)
+}
+
+const createOverlay = () => {
+    if (!document.getElementById("hide-overlay") && document.childNodes[1]) {
+        var newDiv = document.createElement("div");
+        newDiv.setAttribute("id", "hide-overlay");
+        document.childNodes[1].appendChild(newDiv)
+    }
+}
+
+/**
+ * Content to Background communication
+ */
+chrome.runtime.sendMessage({ provide_enabled: "extensionEnabled" }, function (response) {
+    extensionEnabled = response.enabled;
+});
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        setEnabled(request.enabled);
+
+        if (request.enabled === true) {
+            sendResponse({ setting: "enabled" });
+            createOverlay();
+            hidePrice();
+        }
+        else {
+            sendResponse({ setting: "disabled" });
+            overlayOff();
+            showPrice();
+        }
+    });
+
+/**
+ * Extension hide/show
+ */
+createOverlay();
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (extensionEnabled) {
+        hidePrice();
+        overlayOff();
+    }
+});
+
+document.addEventListener('readystatechange', (event) => {
+    if (extensionEnabled) {
+        if (document.readyState !== 'complete') {
+            overlayOn();
+        }
+    }
+});
+
+
